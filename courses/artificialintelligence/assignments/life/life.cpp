25672512e6a4c96422f0c017ba032d3eb7ed3e46
Isaac Hessel-Robinson <isaac.hesselrobinson@mymail.champlain.edu>
2023-09-17 14:04:17 -0400

    --9/17/2023 -The logic now works, I just have to integrate it with proper input

--- courses/artificialintelligence/assignments/life/life.cpp
+++ courses/artificialintelligence/assignments/life/life.cpp
@@ -1,50 +1,129 @@
-#include <fstream>;
-#include <iostream>;
+#include <fstream>
+#include <iostream>
 
 using namespace std;
 
-int main(){
+const int NUM_NEIGHBOURS = 8;
+
+struct Vector2 {
+  Vector2() {}
+  Vector2(int _x, int _y) {
+    x = _x;
+    y = _y;
+  }
+  int x;
+  int y;
+};
+
+struct Cell {
+  char state = '.';
+  char tempState = '.';  // stores the state it will be next turn
+  Vector2 position;      // the position of this cell
+  Cell* neighbours[NUM_NEIGHBOURS] = {nullptr, nullptr, nullptr, nullptr, nullptr,
+                                      nullptr, nullptr, nullptr};  // the positions of neighbours
+  // Cell(){};
+  void CalculateState()  // determines whether to live or die
+  {
+    // counts living neighbors
+    int livingNeighbours = 0;
+    for (int i = 0; i < NUM_NEIGHBOURS; i++) {
+      if (neighbours[i] != nullptr && neighbours[i]->state == '#') {
+        livingNeighbours++;
+      }
+    }
+    // sets state to alive if rules met, otherwise die
+    if (state == '#' && (livingNeighbours == 2 || livingNeighbours == 3)) {
+      tempState = '#';
+    } else if (state == '.' && livingNeighbours == 3) {
+      tempState = '#';
+    } else {
+      tempState = '.';
+    }
+  }
+  void EndOfTurn()  // cleans up at the end of the turn
+  {
+    state = tempState;
+  }
+};
+
+int main() {
   int columns = 1;
-  int lines = 1;
+  int rows = 1;
   int turns = 1;
   int turnsElapsed = 0;
 
-  cout << "hello";
+  // gets input
+  cout << "How many columns will the grid have?";
+  cin >> columns;
+  cout << "How many rows will the grid have?";
+  cin >> rows;
+  cout << "How many turns will the game run for?";
+  cin >> turns;
 
-  char grid[1][1];
-  
-  while (turnsElapsed < turns)
-  {
+  Cell grid[columns][rows];
+  // sets up the cell neighbours
+  for (int r = 0; r < rows; r++) {
+    for (int c = 0; c < columns; c++) {
+      grid[c][r].position = Vector2(c, r);
+      if (c > 0)  // north
+      {
+        grid[c][r].neighbours[0] = &grid[c - 1][r];
+      }
+      if (c > 0 && r < rows - 1)  // northeast
+      {
+        grid[c][r].neighbours[1] = &grid[c - 1][r + 1];
+      }
+      if (r < rows - 1)  // east
+      {
+        grid[c][r].neighbours[2] = &grid[c][r + 1];
+      }
+      if (c < columns - 1 && r < rows - 1)  // southeast
+      {
+        grid[c][r].neighbours[3] = &grid[c + 1][r + 1];
+      }
+      if (c < columns - 1)  // south
+      {
+        grid[c][r].neighbours[4] = &grid[c + 1][r];
+      }
+      if (c < columns - 1 && r > 0)  // southwest
+      {
+        grid[c][r].neighbours[5] = &grid[c + 1][r - 1];
+      }
+      if (r > 0)  // west
+      {
+        grid[c][r].neighbours[6] = &grid[c][r - 1];
+      }
+      if (c > 0 && r > 0)  // northwest
+      {
+        grid[c][r].neighbours[7] = &grid[c - 1][r - 1];
+      }
+    }
   }
 
-  cout << grid;
+  // executes the game
+  while (turnsElapsed < turns) {
+    turnsElapsed++;
+    // every cell figures out what it will do
+    for (int r = 0; r < rows; r++) {
+      for (int c = 0; c < columns; c++) {
+        grid[c][r].CalculateState();
+      }
+    }
+    // every cell now actually does that thing since the buffer is no longer needed
+    for (int r = 0; r < rows; r++) {
+      for (int c = 0; c < columns; c++) {
+        grid[c][r].EndOfTurn();
+      }
+    }
+  }
+
+  // prints the results
+  for (int r = 0; r < rows; r++) {
+    for (int c = 0; c < columns; c++) {
+      cout << grid[c][r].state;
+    }
+    cout << endl;
+  }
 
   return 0;
-};
-
-
-struct Cell {
-	char state = '.';
-	Vector2 position; //the position of this cell
-	Vector2 neighbours[4]; //the positions of neighbours
-	Cell(Vector2 pos) //determines the neighbours
-	{
-		position = pos;
-        neighbours[0] = Vector2(position.x, position.y - 1);
-		neighbours[1] = Vector2(position.x + 1, position.y);
-        neighbours[2] = Vector2(position.x, position.y + 1);
-        neighbours[3] = Vector2(position.x - 1, position.y);
-	}
-};
-
-struct Vector2
-{
-    Vector2() {}
-	Vector2(int _x, int _y)
-	{
-		x = _x;
-		y = _y;
-	}
-	int x;
-	int y;
 };
