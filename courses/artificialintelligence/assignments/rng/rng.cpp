f9f931e884f2fc4faa5e946ef3a8e89d9a70cf62
Isaac HesselRobinson <isaac.hesselrobinson@mymail.champlain.edu>
2023-09-21 13:39:10 -0400

    --9/21/2023 -Started RNG project

--- courses/artificialintelligence/assignments/rng/rng.cpp
+++ courses/artificialintelligence/assignments/rng/rng.cpp
@@ -1,4 +1,31 @@
-// add your imports here
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <bitset>
+#include <cstdint>
+
+using namespace std;
+
 int main(){
-  // code here
+  int32_t value;
+  int32_t numToGen;
+  int min;
+  int max;
+
+  //takes input
+  cin >> value >> numToGen >> min >> max;
+
+  for(int i = 0; i < numToGen; i++)
+  {
+    //shifts the number to 'randomize' it
+    value ^= value << 13;
+
+    value ^= value >> 17;
+
+    value ^= value << 5;
+
+    //clamps the number
+    cout << (value % (max - min + 1)) << endl;
+  }
+
 }
